c4d900b194d85fb8ee92be86b82471b151071c53
Ethan Rudy <31475223+EthanRudy1@users.noreply.github.com>
2021-04-27 10:44:17 -0400

    Updated showcase
    All functions that are completed are used here

--- bigInt_tester.cpp
+++ bigInt_tester.cpp
@@ -3,8 +3,16 @@
 
 using namespace std;
 
-int main(){
-    bigInt num1 = bigInt("112233445566778899");
-    bigInt num2 = bigInt(18);
-    cout << add(num1, num2).toString() << endl;
+int main() {
+	bigInt num1 = bigInt("10");
+	bigInt num2 = bigInt("12");
+	cout << "Showcase: " << num1.toString() << " and " << num2.toString() << endl;
+	cout << "Adding: " << endl;
+	cout << add(num1, num2).toString() << endl;
+	cout << "Subtracting: " << endl;
+	cout << sub(num1, num2).toString() << endl;
+	cout << "Multiplying: " << endl;
+	cout << mult(num1, num2).toString() << endl; 
+	cout << "Power: (num1) squared" << endl;
+	cout << pow(num1, 2).toString() << endl;
 }
