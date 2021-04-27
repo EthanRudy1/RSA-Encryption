667a0655d0568d3a1ee9333422c266062cfcb0d3
Ethan Rudy <31475223+EthanRudy1@users.noreply.github.com>
2021-04-27 10:22:22 -0400

    Update format and added more functionality
    New functions, Subtracting, Multiplication, and methods for testing for less than, greater than, or equal to.

--- bigInt.h
+++ bigInt.h
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <string>
+#include <string.h>
+#include <algorithm>
 
 using namespace std;
 
@@ -48,16 +50,16 @@ class bigInt{
         }
 };
 
- /*
-Adding:
-- How it works: 
-    - So basically, i loop through the longest number (right to left),
-      adding up the digits and then grabbing the remainder
-    - This repeats every iteration until the end
-- Things to keep in mind:
-    - When the larger number is greater than the shorter number,
-      we have to make the other digit value equal to zero in order to not cause an overflow exception
-*/
+bigInt add(bigInt a, bigInt b);
+bigInt sub(bigInt a, bigInt b);
+bigInt mult(bigInt a, bigInt b);
+bigInt div(bigInt a, bigInt b);
+bigInt mod(bigInt a, bigInt b);
+bigInt pow(bigInt a, int b);
+bool equals(bigInt a, bigInt b);
+bool gThan(bigInt a, bigInt b);
+bool lThan(bigInt a, bigInt b);
+// Adds the value of two bigInts
 bigInt add(bigInt a, bigInt b){
     string total = "";
     string num1 = a.toString();
@@ -94,6 +96,175 @@ bigInt add(bigInt a, bigInt b){
         }
         pos2--;
     }
+	if (r > 0){
+		total = to_string(r) + total;
+	}
     bigInt sum = bigInt(total);
     return sum;
+}
+// Subtracts the value of two bigInts
+bigInt sub(bigInt a, bigInt b){
+	string num1 = a.toString();
+	string num2 = b.toString();
+	bool flag = false;
+	if (lThan(a, b)){
+		flag = true;
+		num1 = b.toString();
+		num2 = a.toString();
+	}
+	string total = "";
+	int pos = num2.length() - 1;
+	for (int i = num1.length() - 1; i >= 0; i--){
+		int digi1 = stoi(num1.substr(i, 1));
+		int digi2;
+		if (pos >= 0){
+			digi2 = stoi(num2.substr(pos, 1));
+		} else {
+			digi2 = 0;
+		}
+		pos--;
+		int diff;
+		if (digi1 > digi2){
+			diff = digi1 - digi2;
+		} else if (digi1 == digi2){
+			diff = 0;
+		} else {
+			num1 = num1.substr(0, i - 1) + to_string(stoi(num1.substr(i - 1, 1)) - 1) + num1.substr(i, num1.length() - i);
+			digi1 += 10;
+			diff = digi1 - digi2;
+		}
+		total = to_string(diff) + total; 
+	}
+	int p = 0;
+	while (true){
+		if (total.substr(p, 1) == "0"){
+			total.erase(p, 1);
+			p--;
+		} else {
+			break;
+		}
+		p++;
+	}
+	if (flag){
+		total = "-" + total;
+	}
+	bigInt ans = bigInt(total);
+	return ans;
+}
+// Multiplies the value of two bigInts
+bigInt mult(bigInt a, bigInt b){
+	bigInt total = (0);
+	string num1 = a.toString();
+	string num2 = b.toString();
+	int l1 = num1.length();
+	int l2 = num2.length();
+	if (l1 < l2){
+		string temp = num1;
+		num1 = num2;
+		num2 = temp;
+		int lenTemp = 1;
+		l1 = l2;
+		l2 = lenTemp;
+	}
+	int r = 0;
+	int zerCount = 0;
+	for (int i = num2.length() - 1; i >= 0; i--){
+		string colSum = "";
+		for (int z = 0; z < zerCount; z++){
+			colSum = "0" + colSum;
+		}
+		for (int ii = num1.length() - 1; ii >= 0; ii--){
+			string d1 = num2.substr(i, 1);
+			string d2 = num1.substr(ii, 1);
+			int digiSum = (stoi(d1) * stoi(d2)) + r;
+			if (to_string(digiSum).length() > 1){
+				string t = to_string(digiSum);
+				r = stoi(t.substr(0, t.length() - 1));
+				string subDigi = t.substr(t.length() - 1, 1);
+				colSum = subDigi + colSum;
+			} else {
+				colSum = to_string(digiSum) + colSum;
+				r = 0;
+			}
+		}
+		bigInt col = bigInt(colSum);
+		total = add(total, col);
+	}
+	if (r > 0){
+		total = bigInt(to_string(r) + total.toString());
+	}
+	return total;
+}
+
+bigInt div(bigInt a, bigInt b){
+
+
+
+	bigInt sad = bigInt(0);
+	return sad;
+}
+
+bigInt mod(bigInt a, bigInt b){
+
+
+
+	bigInt sad = bigInt(0);
+	return sad;
+}
+
+bigInt pow(bigInt a, int b){
+	bigInt sad = bigInt("0");
+	return sad;
+}
+// Tests if two bigInts are of equal value
+bool equals(bigInt a, bigInt b){
+	if (a.toString().length() != b.toString().length()){
+		return false;
+	}
+	for (int i = 0; i < a.toString().length(); i++){
+		int d1 = stoi(a.toString().substr(i,1));
+		int d2 = stoi(b.toString().substr(i,1));
+		if (d1 != d2){
+			return false;
+		}
+	}
+	return true;
+}
+// Tests if the the bigInt "a" is greater than bigInt "b"
+bool gThan(bigInt a, bigInt b){
+	if (a.toString().length() > b.toString().length()){
+		return true;
+	} else if (a.toString().length() < b.toString().length()){
+		return false;
+	}
+	for (int i = 0; i < a.toString().length(); i++){
+		int d1 = stoi(a.toString().substr(i, 1));
+		int d2 = stoi(b.toString().substr(i, 1));
+		if (d1 < d2){
+			return false;
+		}
+	}
+	if (!equals(a, b)){
+		return true;
+	}
+	return true;
+}
+// Tests if the bigInt "a" is less than bigInt "b"
+bool lThan(bigInt a, bigInt b){
+	if (a.toString().length() < b.toString().length()){
+		return true;
+	} else if (a.toString().length() > b.toString().length()){
+		return false;
+	}
+	for (int i = 0; i < b.toString().length(); i++){
+		int d1 = stoi(a.toString().substr(i, 1));
+		int d2 = stoi(b.toString().substr(i, 1));
+		if (d1 > d2){
+			return false;
+		}
+	}
+	if (!equals(a, b)){
+		return true;
+	}
+	return true;
 }
